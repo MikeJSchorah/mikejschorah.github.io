8b1f98f02a330c4ee790957361c1f8524747f3a8
Mike <mikejschorah@gmail.com>
2017-01-26 18:48:41 -0500

    Create script.js

---
+++ prism-enterprises/resources/js/script.js
@@ -0,0 +1,28 @@
+// SMOOTH SCROLL
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
+    }
+  });
+});
+
+$(document).ready(function(){
+
+  jQuery('.header-images').slippry( {
+    captions: false,
+    responsive: true,
+    pager: false,
+    controls: false,
+    speed: 4000,
+    autoHover: false
+  });
+
+});
